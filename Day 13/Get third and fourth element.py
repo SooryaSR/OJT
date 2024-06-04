bcd11f14c9f8035137b4f8410457cadd9b25ac58
Soorya S <122440143+SooryaSR@users.noreply.github.com>
2024-06-04 16:54:43 +0530

    Add files via upload

---
+++ Day 13/Get third and fourth element.py
@@ -0,0 +1,14 @@
+#Get third and fourth elements from an array and add them
+
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+third_element = arr[2]
+fourth_element = arr[3]
+
+result = third_element + fourth_element
+
+print("Third element:", third_element)
+print("Fourth element:", fourth_element)
+print("Sum of third and fourth elements:", result)
---
+++ Day 13/third element od second array of the first array.py
@@ -0,0 +1,6 @@
+#Access the third element of the second array of the first array
+import numpy as np
+
+arr = np.array([[1, 2, 3, 4, 0], [5, 6, 7, 8, 9]])
+element = arr[1, 4]
+print(element)
